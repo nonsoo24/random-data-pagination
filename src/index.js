356431ded382334d9d2462352c11633c3ea25bf5
Daniel Chukwurah <danielchukwurah@Nonso-MacBook-Pro.local>
2022-09-01 15:18:09 +0100

    chore: modify index.js

--- src/index.js
+++ src/index.js
@@ -102,7 +102,7 @@ const getPreviousKey = () => {
 
 const createTable = (data) => {
     const tableRow = data.map((item) => {
-        return `<tr data-entryid=${item.id}>
+        return `<tr data-entryid="${item.id}">
                     <td>${item.row}</td>
                     <td>${item.gender.toUpperCase()}</td>
                     <td>${item.age}</td>
